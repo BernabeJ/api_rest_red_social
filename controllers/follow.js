3e13ee94d0a0d62efa068bc51ba08e8932d6ac2f
Bernabé <bernabe.j.j@gmail.com>
2023-04-19 19:36:24 +0200

    metodos de follow

--- controllers/follow.js
+++ controllers/follow.js
@@ -1,3 +1,11 @@
+//importa modelo
+const Follow = require("../models/follow");
+const User = require("../models/user");
+
+//Importa dependencias
+
+const mongoosePaginate = require("mongoose-pagination");
+
 //Acciones de prueba
 const pruebaFollow = (req, res) => {
   return res.status(200).send({
@@ -5,7 +13,103 @@ const pruebaFollow = (req, res) => {
   });
 };
 
+//Accion de seguir
+const save = (req, res) => {
+  //Conseguir datos por body
+  const params = req.body;
+
+  //sacar id del usuario identificado
+  const identity = req.user;
+
+  //crear objeto con modelo follow
+  let userToFollow = new Follow({
+    user: identity.id,
+    followed: params.followed,
+  });
+
+  //Guardar objeto en base de datos
+  userToFollow.save((error, followStored) => {
+    if (error || !followStored) {
+      return res.status(500).send({
+        status: "error",
+        message: "No se ha podido seguir al usuario",
+      });
+    }
+    return res.status(200).send({
+      status: "succes",
+      message: "Metodo dar follow",
+      identity: req.user,
+      follow: followStored,
+    });
+  });
+};
+//Accion de dejar de seguir
+const unfollow = (req, res) => {
+  //Recoger el id del usuario identificado
+  const userId = req.user.id;
+
+  //Recoger el id del usuario que quiero dejar de seguir
+  const followedId = req.params.id;
+
+  //Find de las coincidencias y hacer remove
+  Follow.find({
+    user: userId,
+    followed: followedId,
+  }).remove((error, followDeleted) => {
+    if (error || !followDeleted) {
+      return res.status(500).send({
+        stauts: "error",
+        message: "no has deajdo de seguir a nadie",
+      });
+    }
+
+    return res.status(200).send({
+      message: "Follow eliminado correctamente",
+      status: "success",
+    });
+  });
+};
+//Listado de usuarios que estoy siguiendo
+const following = (req, res) => {
+  //Sacar el usuairo identificado
+  let userId = req.user.id;
+
+  //Comprobar si me llega el id por parametro en url
+  if (req.params.id) {
+    userId = req.params.id;
+  }
+  //Comprobar si me llega la pagina
+  let page = 1;
+  if (req.params.page) page = req.params.page;
+  //Usuarios por pagina que quiero mostrar
+  const itemsPerPage = 5;
+  //FInd a follow, popular datos de los usuarios y paginar con mongoose paginate
+  Follow.find({ user: userId })
+    .populate("user followed", "-password -role -__v")
+    .paginate(page, itemsPerPage, (error, follows, total) => {
+      //Sacar un array de ids de usuarios que me siguen y que sigo
+
+      return res.status(200).send({
+        status: "succes",
+        message: "Listado de usuarios que estoy siguiendo",
+        follows,
+        total,
+        pages: Math.ceil(total / itemsPerPage),
+      });
+    });
+};
+//Listado de usuarios que nos siguen
+const followers = (req, res) => {
+  return res.status(200).send({
+    status: "succes",
+    message: "Listado de usuarios que me siguen",
+  });
+};
 //Exportar acciones
 module.exports = {
   pruebaFollow,
+  save,
+  unfollow,
+  following,
+  followers,
 };
--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,14 @@
 //Importar dependecias y modulos
 const bcrypt = require("bcrypt");
+const mongoosePagination = require("mongoose-pagination");
+const user = require("../models/user");
+const fs = require("fs");
+const path = require("path");
+//Importar modelos
 const User = require("../models/user");
 //Importa servicios
 const jwt = require("../services/jwt");
+const { exists } = require("../models/user");
 
 //Acciones de prueba
 const pruebaUser = (req, res) => {
@@ -112,9 +118,215 @@ const login = (req, res) => {
     });
 };
 
+const profile = (req, res) => {
+  //Recibir el parámetro del id del usuario por la url
+  const id = req.params.id;
+
+  //Consulta para sacar los datos del usuario
+  User.findById(id)
+    .select({ password: 0, role: 0 })
+    .exec((error, userProfile) => {
+      if (error || !userProfile) {
+        return res.status(404).send({
+          status: "error",
+          message: "El usuario no existe o hay un error",
+        });
+      }
+      //Posteriormente: devolver informacion de follows
+      return res.status(200).send({
+        status: "succes",
+        user: userProfile,
+      });
+    });
+  //Devolver resultado
+};
+
+const list = (req, res) => {
+  //Controlar en que pagina estamos
+  let page = 1;
+  if (req.params.page) {
+    page = req.params.page;
+  }
+  page = parseInt(page);
+
+  //Consulta con mongoose paginate
+  let itemsPerPage = 4;
+
+  User.find()
+    .sort("_id")
+    .paginate(page, itemsPerPage, (error, users, total) => {
+      //Devolver resultado(posteriormente info de follows)
+
+      if (error || !users) {
+        return res.status(404).send({
+          status: "error",
+          message: "No hay usuarios disponibles",
+          page,
+        });
+      }
+      return res.status(200).send({
+        status: "succes",
+        users,
+        page,
+        itemsPerPage,
+        total,
+        pages: Math.ceil(total / itemsPerPage),
+      });
+    });
+};
+
+const update = (req, res) => {
+  //Recoger info de usuario a actualizar
+  const userIdentity = req.user;
+  let userToUpdate = req.body;
+
+  //Eliminar campos sobrantes
+  delete userToUpdate.iat;
+  delete userToUpdate.exp;
+  delete userToUpdate.role;
+  delete userToUpdate.image;
+  //Comprobar si el usuario ya existe
+  User.find({
+    $or: [
+      { email: userToUpdate.email.toLowerCase() },
+      { nick: userToUpdate.email.toLowerCase() },
+    ],
+  }).exec(async (error, users) => {
+    if (error)
+      return res.status(500).json({
+        status: "error",
+        message: "error en la consulta de usuarios",
+      });
+
+    let userIsset = false;
+    users.forEach((user) => {
+      if (user && user._id != userIdentity.id) userIsset = true;
+    });
+
+    if (userIsset) {
+      return res.status(200).send({
+        status: "succes",
+        message: "El usuario ya existe",
+      });
+    }
+
+    //Cifrar la contraseña
+    if (userToUpdate.password) {
+      let pwd = await bcrypt.hash(userToUpdate.password, 10);
+      userToUpdate.password = pwd;
+    }
+    //Buscar y actualizar
+
+    try {
+      let userUpdated = await User.findByIdAndUpdate(
+        { _id: userIdentity.id },
+        userToUpdate,
+        { new: true }
+      );
+      if (!userUpdated) {
+        return res.status(400).json({
+          status: "error",
+          message: "Error al actualizar  usuario",
+        });
+      }
+
+      //Devolver respuesta
+      return res.status(200).send({
+        status: "succes",
+        message: "Metodo de actualizar usuario ",
+        user: userUpdated,
+      });
+    } catch (error) {
+      return res.status(500).send({
+        status: "error",
+        message: "Error al actualizar el usuario ",
+      });
+    }
+  });
+};
+
+const upload = (req, res) => {
+  //recoger el fichero de imagen y comprobar que existe
+  if (!req.file) {
+    return res.status(404).send({
+      status: "error",
+      message: "Peticion no incluye la imagen",
+    });
+  }
+
+  //conseguir el nombre del archivo
+  let image = req.file.originalname;
+  //Sacar la extension del archivo
+  const imageSplit = image.split(".");
+  const extension = imageSplit[1];
+
+  //Comprobar extension
+  if (
+    extension != "png" &&
+    extension != "jpg" &&
+    extension != "jpeg" &&
+    extension != "gif"
+  ) {
+    //Borrrar archivo
+    const filePath = req.file.path;
+    const fileDeleted = fs.unlinkSync(filePath);
+    return res.status(400).send({
+      status: "error",
+      message: "Extension del fichero no valida",
+    });
+  }
+
+  //Si si es correcta, guardar imagen en bbdd
+  User.findOneAndUpdate(
+    { _id: req.user.id },
+    { image: req.file.filename },
+    { new: true },
+    (error, userUpdate) => {
+      if (error || !userUpdate) {
+        return res.status(500).send({
+          status: "error",
+          message: "Error en la subida del avatar",
+        });
+      }
+      return res.status(200).send({
+        status: "success",
+        user: userUpdate,
+        file: req.file,
+      });
+    }
+  );
+};
+
+const avatar = (req, res) => {
+  //Sacar el parametro de la url
+  const file = req.params.file;
+
+  //Montar un path real de la imagen
+  const filePath = "./uploads/avatars/" + file;
+  console.log(filePath);
+
+  //Comprobar que el archivo existe
+  fs.stat(filePath, (error, exists) => {
+    if (!exists) {
+      return res.status(404).send({
+        status: "error",
+        message: "No existe la imagen",
+      });
+    }
+    //Devolver un file
+
+    return res.sendFile(path.resolve(filePath));
+  });
+};
+
 //Exportar acciones
 module.exports = {
   pruebaUser,
   register,
   login,
+  profile,
+  list,
+  update,
+  upload,
+  avatar,
 };
